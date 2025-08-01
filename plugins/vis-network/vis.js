fa68f2b4e91dad5122d6c85c6b30dab286e96a81
Cameron Fischer <fischer.cameron@gmail.com>
2025-07-31 23:46:08 -0400

    Fix for ResizeObserver loop error
    We swallow the problem, just like our anger.

--- plugins/vis-network/vis.js
+++ plugins/vis-network/vis.js
@@ -24,6 +24,14 @@ if($tw.boot.tasks.trapErrors) {
       var text = "Strange firefox related vis.js error (see #163)";
       console.error(text, arguments);
       
+    } else if(errorMsg.indexOf("ResizeObserver loop") !== -1) {
+      // Issue appears sporatically. Hard to replicate. Site here indicates
+      // these errors can be safely ignored.
+      // https://stackoverflow.com/questions/49384120/resizeobserver-loop-limit-exceeded/50387233#50387233
+      // variations of this harmless error:
+      //   ResizeObserver loop limit exceeded
+      //   ResizeObserver loop completed with undelivered notifications
+      console.error(errorMsg, arguments);
     } else if(defaultHandler) {
       
       defaultHandler.apply(this, arguments);
