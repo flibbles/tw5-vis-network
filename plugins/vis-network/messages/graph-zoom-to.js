841478f66b81ea344e8591064689267e3d7c2b70
Cameron Fischer <fischer.cameron@gmail.com>
2025-08-29 22:58:47 -0400

    Exposing less with this graph-zoom-to message

--- plugins/vis-network/messages/graph-zoom-to.js
+++ plugins/vis-network/messages/graph-zoom-to.js
@@ -6,6 +6,8 @@ exports.parameters = {
 	x: {type: "number"},
 	y: {type: "number"},
 	zoom: {type: "number"},
+	/* For now, we're not going to expose the animation features.
+	 * It's easier to add them later than to remove them
 	ms: {type: "number"},
 	easingFunction: {type: "enum", default: "linear", values: [
 		"linear",
@@ -22,6 +24,7 @@ exports.parameters = {
 		"easeOutQuint",
 		"easeInOutQuint"
 	]}
+	*/
 };
 
 exports.handle = function(message, params) {
@@ -33,10 +36,13 @@ exports.handle = function(message, params) {
 		var scale = this.vis.getScale();
 		settings.scale = scale * parseFloat(params.zoom);
 	}
+	/*
 	if (params.ms !== undefined) {
 		settings.animation = {
 				duration: parseFloat(params.time || 0),
 				easingFunction: params.easeFunction || "linear"};
 	}
+	*/
+	settings.animation = true;
 	this.vis.moveTo(settings);
 };
--- tiddlers/messages.js
+++ tiddlers/messages.js
@@ -16,11 +16,11 @@ function element() {
 	return $tw.fakeDocument.createElement("div");
 };
 
-fit("can do doubleclick for graphs", function() {
+it("can do doubleclick for graphs", function() {
 	adapter.init(element(), {});
 	var moveTo = spyOn(adapter.output, "moveTo");
 	adapter.handleMessage({type: "graph-zoom-to"}, {x: 14, y: 17});
-	expect(moveTo).toHaveBeenCalledWith({position: {x: 14, y: 17}});
+	expect(moveTo).toHaveBeenCalledWith({position: {x: 14, y: 17}, animation: true});
 });
 
 });
